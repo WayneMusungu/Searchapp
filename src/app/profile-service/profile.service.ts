9d7e378773dfe686eda7f101418b6aea3fef199d
WayneMusungu <waynemuyera17@gmail.com>
2022-04-18 14:57:32 +0300

    Add implicity any false

---
+++ src/app/profile-service/profile.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+// import { Http, Headers} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor() { }
+}
