5c448751538b0037c1640905ab6ea2e138be1a97
WayneMusungu <waynemuyera17@gmail.com>
2022-04-18 22:31:00 +0300

    Add css styles

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -25,7 +25,7 @@ export class ProfileComponent implements OnInit {
     this.profileService.getProfileInfo().subscribe(profile =>{
       console.log(profile);
       this.profile=profile;
-    })
+    }), 
     this.profileService.getProfileRepos().subscribe(repos=>{
       console.log(repos);
       this.repos= repos;
